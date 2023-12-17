52cdcd5b8dd4ce1326aca672988261900a38bb3a
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-17 04:16:10 +0200

    Update player movement script

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using UnityEditor;
 using UnityEngine;
 
-public class Control : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Control : MonoBehaviour {
+
+    public new Rigidbody rigidbody;
+
+    public float speed = 600f;
+    public float glide = 400f;
+
+    private bool keyL = false;
+    private bool keyR = false;
+    
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        keyL = Input.GetKey(KeyCode.A) ? true : false;
+        keyR = Input.GetKey(KeyCode.D) ? true : false;
+    }
+
+    void FixedUpdate() {
         
+        rigidbody.AddForce(0, 0, speed * Time.deltaTime);
+        if (keyL) rigidbody.AddForce(-glide * Time.deltaTime, 0, 0);
+        if (keyR) rigidbody.AddForce(+glide * Time.deltaTime, 0, 0);
     }
 }
