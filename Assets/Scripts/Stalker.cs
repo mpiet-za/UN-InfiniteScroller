023e9375ac913f0766dd72f64539f8f97d19d9ff
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-17 04:34:12 +0200

    Apply stalker script to camera object

--- Assets/Scripts/Stalker.cs
+++ Assets/Scripts/Stalker.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using UnityEditor;
 using UnityEngine;
 
-public class Follower : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Stalker : MonoBehaviour {
+    
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
